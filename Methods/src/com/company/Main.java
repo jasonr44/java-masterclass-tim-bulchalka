6923828f6e04d967241edb720bfcb2f4fe49dd06
Jason Ransom <jasonlawrenceransom@gmail.com>
2020-11-25 06:50:23 -0500

    Added some method overloading.

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -6,16 +6,20 @@ public class Main {
     private static final boolean notCool = false;
 
     public static void main(String[] args) {
-	    int result = calculateScore(2);
-	    if (result > 100) {
-	        cool = true;
-        }
-
-        System.out.println(tellMeTheScore(result));
-
-	    SpeedConverter converter = new SpeedConverter();
-
-	    converter.printConversion(10.25);
+//	    int result = calculateScore(2);
+//	    if (result > 100) {
+//	        cool = true;
+//        }
+//
+//        System.out.println(tellMeTheScore(result));
+//
+//	    SpeedConverter converter = new SpeedConverter();
+//
+//	    converter.printConversion(10.25);
+        int newScore = calculateScore("Jason", 500);
+        System.out.println("New score is " + newScore);
+        int unamedPlayerScore = calculateScore(200);
+        System.out.println("Unamed score is " + unamedPlayerScore);
     }
 
     public static int calculateScore(Integer input) {
@@ -24,5 +28,15 @@ public class Main {
 
     public static String tellMeTheScore(Integer input) {
         return (input + " and " + cool + " and not cool " + notCool);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unamed player scored " + score + " points");
+        return score * 1000;
     }
 }
