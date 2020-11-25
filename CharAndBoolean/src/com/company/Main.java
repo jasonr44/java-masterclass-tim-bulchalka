ca7f0317cbf4412470d8f9cc40cb5e389b7588ae
Jason Ransom <jasonlawrenceransom@gmail.com>
2020-11-24 20:55:41 -0500

    Chars and Booleans

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Chars
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyRightChar = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyRightChar);
+
+        // Booleans
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
